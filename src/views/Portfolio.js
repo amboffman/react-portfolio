ca30bfd7dd54f170df7633450691ca816f9476ac
Anthony Boffman <amarrokalboffman@gmail.com>
2020-10-10 19:46:37 -0700

    Portfolio UI

--- src/views/Portfolio.js
+++ src/views/Portfolio.js
@@ -6,67 +6,73 @@ import Button from "react-bootstrap/Button";
 
 function Portfolio() {
   return (
-    <Container fluid style={{paddingTop: 40, paddingBottom: 40, borderTop: "solid", borderBottom: "solid"}}>
-       <h5>Projects</h5>
-            <p>
-<strong>506</strong> contributions and counting!
-            </p>
-      <Row style={{justifyContent: "space-around", }}>
-        <Card style={{ width: "15rem" }}>
-        <Card.Img
-          variant="top"
-          src="https://www.mountsinai.on.ca/wellbeing/images/image-placeholder/image_preview"
-        />
-        <Card.Body>
-          <Card.Title>Gamersaurus</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
+    <Container
+      fluid
+      style={{
+        padding:40,
+        borderTop: "solid",
+        borderBottom: "solid",
+      }}
+    >
+      <h2 style={{textAlign: 'center'}}>Projects</h2>
+      <p style={{textAlign: 'center', paddingBottom: 20}}>
+        <strong>506</strong> contributions and counting!
+      </p>
+      <Row style={{ justifyContent: "space-around" }}>
+        <Card bg={"primary"} text={'white'}style={{ width: "15rem" }}>
+          <Card.Img
+            variant="top"
+            src="https://www.mountsinai.on.ca/wellbeing/images/image-placeholder/image_preview"
+          />
+          <Card.Header>Gamersaurus</Card.Header>
+          <Card.Body>
+            <Card.Text style={{ height: 100 }}>
+              A videogame webapp for users to search games, see the latest
+              trends, and save personal libraries.
+            </Card.Text>
+            <Button variant="light">View App</Button>
+          </Card.Body>
         </Card>
-        <Card style={{ width: "15rem" }}>
-        <Card.Img
-          variant="top"
-          src="https://www.mountsinai.on.ca/wellbeing/images/image-placeholder/image_preview"
-        />
-        <Card.Body>
-          <Card.Title>Wanderful</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
+        <Card bg={"warning"} text={'white'} style={{ width: "15rem" }}>
+          <Card.Img
+            variant="top"
+            src="https://www.mountsinai.on.ca/wellbeing/images/image-placeholder/image_preview"
+          />
+          <Card.Header>Wanderful</Card.Header>
+          <Card.Body>
+            <Card.Text style={{ height: 100 }}>
+              Log destinations to track destinations you've visited and build
+              your wishlist.
+            </Card.Text>
+            <Button variant="light">View App</Button>
+          </Card.Body>
         </Card>
-        <Card style={{ width: "15rem" }}>
-        <Card.Img
-          variant="top"
-          src="https://www.mountsinai.on.ca/wellbeing/images/image-placeholder/image_preview"
-        />
-        <Card.Body>
-          <Card.Title>Moodies</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
+        <Card bg={"success"} text={'white'} style={{ width: "15rem" }}>
+          <Card.Img
+            variant="top"
+            src="https://www.mountsinai.on.ca/wellbeing/images/image-placeholder/image_preview"
+          />
+          <Card.Header>Moodies</Card.Header>
+          <Card.Body>
+            <Card.Text style={{ height: 100 }}>
+              A restaurant app that recommends local cuisine based on the user's
+              mood.
+            </Card.Text>
+            <Button variant="light">View App</Button>
+          </Card.Body>
         </Card>
-        <Card style={{ width: "15rem" }}>
-        <Card.Img
-          variant="top"
-          src="https://www.mountsinai.on.ca/wellbeing/images/image-placeholder/image_preview"
-        />
-        <Card.Body>
-          <Card.Title>Victorise</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
+        <Card bg={"info"} text={'white'} style={{ width: "15rem" }}>
+          <Card.Img
+            variant="top"
+            src="https://www.mountsinai.on.ca/wellbeing/images/image-placeholder/image_preview"
+          />
+          <Card.Header>Victorise</Card.Header>
+          <Card.Body>
+            <Card.Text style={{ height: 100 }}>
+              An activity monitoring app available on the Apple store.
+            </Card.Text>
+            <Button variant="light">View App</Button>
+          </Card.Body>
         </Card>
       </Row>
     </Container>
