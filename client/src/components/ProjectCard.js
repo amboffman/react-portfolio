2c1d3681309f5847bf13d1f88dd7f7c674a08108
Anthony Boffman <amarrokalboffman@gmail.com>
2020-12-01 13:53:35 -0800

    Feat animates project card hover effect

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useLayoutEffect } from "react";
 import Card from "react-bootstrap/Card";
 import {
   GiRetroController,
@@ -7,19 +7,38 @@ import {
   GiPathDistance,
 } from "react-icons/gi";
 import Button from "react-bootstrap/Button";
+import { useSpring, animated } from "react-spring";
 
 function ProjectCard(props) {
-  const [hover, setHover] = useState(false);
+  const targetRef = useRef();
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  useLayoutEffect(() => {
+    if (targetRef.current) {
+      setDimensions({
+        width: targetRef.current.offsetWidth,
+        height: targetRef.current.offsetHeight,
+      });
+    }
+  }, []);
+
+  const calc = (x, y) => [
+    -(y - window.innerHeight / 2) / dimensions.height,
+    (x - window.innerWidth / 2) / dimensions.width,
+    1.1,
+  ];
+  const trans = (x, y, s) =>
+    `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+  const [angle, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
+
   function icon(icon) {
     switch (icon) {
       case "controller":
         return (
           <GiRetroController
-            style={
-              hover
-                ? { fontSize: 75, alignSelf: "center" }
-                : { fontSize: 50, alignSelf: "center" }
-            }
             onClick={() => {
               window.open(props.link, "_blank");
             }}
@@ -28,11 +47,6 @@ function ProjectCard(props) {
       case "globe":
         return (
           <GiGlobe
-            style={
-              hover
-                ? { fontSize: 75, alignSelf: "center" }
-                : { fontSize: 50, alignSelf: "center" }
-            }
             onClick={() => {
               window.open(props.link, "_blank");
             }}
@@ -41,11 +55,6 @@ function ProjectCard(props) {
       case "food":
         return (
           <GiFoodTruck
-            style={
-              hover
-                ? { fontSize: 75, alignSelf: "center" }
-                : { fontSize: 50, alignSelf: "center" }
-            }
             onClick={() => {
               window.open(props.link, "_blank");
             }}
@@ -54,11 +63,6 @@ function ProjectCard(props) {
       case "gps":
         return (
           <GiPathDistance
-            style={
-              hover
-                ? { fontSize: 75, alignSelf: "center" }
-                : { fontSize: 50, alignSelf: "center" }
-            }
             onClick={() => {
               window.open(props.link, "_blank");
             }}
@@ -67,11 +71,6 @@ function ProjectCard(props) {
       default:
         return (
           <GiPathDistance
-            style={
-              hover
-                ? { fontSize: 75, alignSelf: "center" }
-                : { fontSize: 50, alignSelf: "center" }
-            }
             onClick={() => {
               window.open(props.link, "_blank");
             }}
@@ -80,37 +79,52 @@ function ProjectCard(props) {
     }
   }
   return (
-    <Card
-      bg={props.color}
-      text={"white"}
-      style={{ width: "15rem", marginBottom: 30 }}
+    <animated.div
+      onMouseMove={({ clientX: x, clientY: y }) => {
+        set({ xys: calc(x, y) });
+      }}
+      onMouseLeave={() => {
+        set({ xys: [0, 0, 1] });
+      }}
+      style={{ transform: angle.xys.interpolate(trans) }}
     >
-      <Card.Title
-        href={props.link}
-        onMouseEnter={() => {
-          setHover(true);
-        }}
-        onMouseLeave={() => {
-          setHover(false);
-        }}
-        style={{
-          height: 100,
-          textAlign: "center",
-          margin: "auto",
-          paddingTop: 15,
-          fontSize: 50,
-        }}
+      <Card
+        bg={props.color}
+        text={"white"}
+        style={{ width: "15rem", marginBottom: 30 }}
+        ref={targetRef}
       >
-        {icon(props.icon)}
-      </Card.Title>
-      <Card.Header>{props.title}</Card.Header>
-      <Card.Body>
-        <Card.Text style={{ height: 100 }}>{props.description}</Card.Text>
-        <Button variant="light" href={props.link} target="_blank">
-          View App
-        </Button>
-      </Card.Body>
-    </Card>
+        <Card.Title
+          href={props.link}
+          style={{
+            height: 100,
+            textAlign: "center",
+            margin: "auto",
+            paddingTop: 15,
+            fontSize: 50,
+          }}
+        >
+          <animated.div
+            onMouseMove={({ clientX: x, clientY: y }) => {
+              set({ xys: calc(x, y) });
+            }}
+            onMouseLeave={() => {
+              set({ xys: [0, 0, 1] });
+            }}
+            style={{ transform: angle.xys.interpolate(trans) }}
+          >
+            {icon(props.icon)}
+          </animated.div>
+        </Card.Title>
+        <Card.Header>{props.title}</Card.Header>
+        <Card.Body>
+          <Card.Text style={{ height: 100 }}>{props.description}</Card.Text>
+          <Button variant="light" href={props.link} target="_blank">
+            View App
+          </Button>
+        </Card.Body>
+      </Card>
+    </animated.div>
   );
 }
 
